b612fb6107001efdb28a42d58fd8da399fb8d92d
sapthagirikrishnan <svtiles14@gmail.com>
2026-07-31 16:33:46 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import GSTBillingApp from "./App.jsx";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GSTBillingApp />
-  </React.StrictMode>
-);
